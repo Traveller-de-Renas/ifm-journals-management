757576b77e94b9205316ffabcc160046a8d2bcec
Mathias R. Kiheka <36520184+Traveller-de-Renas@users.noreply.github.com>
2024-12-03 09:15:12 -0800

    Update reviewer_notification.blade.php

--- resources/views/mail/reviewer_notification.blade.php
+++ resources/views/mail/reviewer_notification.blade.php
@@ -3,12 +3,15 @@
 @endphp
 Hello, <b>{{ $user->salutation?->title }}. {{ $user->last_name }}</b>
 <br>
-We are requesting your review for the article: <b><h4>{{ $record->title }}<h4></b><br>
-Submited in the Journal: <b><h4>{{ $record->journal->title }}</h4></b>
+<br>
+We are requesting your review on the article: <b>{{ $record->title }}</b>
+<br>
+Submited in the Journal: <b>{{ $record->journal->title }}</b>
 <br>
 <br>
 
 Please find the Review Link / Evaluation Form, Click on the link to access the form : <a href="{{ route('journals.article_evaluation', [$record->uuid, $user->uuid]) }}" ><u>{{ $record->title }}</u></a>
+<br>
 we expect you will reach out to us if you have any questions or concerns. 
 <br>
 <br>
@@ -16,4 +19,4 @@ From chief Editor of the Journal Editorial Board.
 <br>
 Regards,
 <br>
-{{ $record->journal->chief_editor->salutation?->title }} {{ $record->journal->chief_editor->first_name }} {{ $record->journal->chief_editor->middle_name }} {{ $record->journal->chief_editor->last_name }}
+{{ $record->journal->chief_editor->salutation?->title }}. {{ $record->journal->chief_editor->first_name }} {{ $record->journal->chief_editor->middle_name }} {{ $record->journal->chief_editor->last_name }}
